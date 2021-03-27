600788d38230f237105c6ac5e1f6c0e490ee9b7f
freitasJV <jose.freitas16@fatec.sp.gov.br>
2021-03-27 12:30:19 -0300

    Obtendo os dados do formulario de login

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, MenuController, NavController } from 'ionic-angular';
+import { CredenciaisDTO } from '../../models/credenciais.dto';
 
 @IonicPage()
 @Component({
@@ -7,6 +8,11 @@ import { IonicPage, MenuController, NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+
+  creds: CredenciaisDTO = {
+    email: "",
+    senha: ""
+  };
 
   constructor(public navCtrl: NavController, public menu: MenuController) {
 
@@ -21,6 +27,7 @@ export class HomePage {
   }
 
   login(){
+    console.log(this.creds);
     this.navCtrl.setRoot('CategoriasPage')
   }
 }
