9688379f31885e6790e1c4cf3914fdcf1eeb8b11
freitasJV <jose.freitas16@fatec.sp.gov.br>
2021-03-27 12:45:16 -0300

    Comecando a implementar a autenticacao

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, MenuController, NavController } from 'ionic-angular';
 import { CredenciaisDTO } from '../../models/credenciais.dto';
+import { AuthService } from '../../services/auth.service';
 
 @IonicPage()
 @Component({
@@ -14,7 +15,10 @@ export class HomePage {
     senha: ""
   };
 
-  constructor(public navCtrl: NavController, public menu: MenuController) {
+  constructor(
+    public navCtrl: NavController, 
+    public menu: MenuController,
+    public auth: AuthService) {
 
   }
 
@@ -26,8 +30,14 @@ export class HomePage {
     this.menu.swipeEnable(true);
   }
 
-  login(){
+  login() {
     console.log(this.creds);
-    this.navCtrl.setRoot('CategoriasPage')
+    //this.navCtrl.setRoot('CategoriasPage');
+    this.auth.authenticate(this.creds)
+      .subscribe(response => {
+        console.log(response.headers.get('Authorization'));
+        this.navCtrl.setRoot('CategoriasPage');
+      },
+      error => {});    
   }
 }
