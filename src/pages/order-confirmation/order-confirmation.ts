bb4d02dc53feadad606f39b4be7dac1c844d39f9
freitasJV <jose.freitas16@fatec.sp.gov.br>
2021-04-07 09:50:04 -0300

    Informando pedido registrado ao usuario

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -19,6 +19,7 @@ export class OrderConfirmationPage {
   cartItems: CartItem[];
   cliente: ClienteDTO;
   endereco: EnderecoDTO;
+  codpedido: string;
 
   constructor(
     public navCtrl: NavController, 
@@ -53,17 +54,26 @@ export class OrderConfirmationPage {
   back(){
     this.navCtrl.setRoot('CartPage');
   }
+
+  home(){
+    this.navCtrl.setRoot('CategoriasPage');
+  }
   
   checkout(){
     this.pedidoService.insert(this.pedido)
       .subscribe(response => {
         this.cartService.createOrClearCart();
-        console.log(response.headers.get('location'));
+        this.codpedido = this.extractId(response.headers.get('location'));
       },
       error => {
         if(error.status == 403){
           this.navCtrl.setRoot('HomePage');
         }
       });
+  }
+
+  private extractId(location : string): string {
+    let position = location.lastIndexOf('/');
+    return location.substring(position + 1, location.length);
   }
 }
